ce06cdc2a0c5a84564e369d5a707db76e5ef18df
rizvan99 <rizvan2312@hotmail.com>
2021-04-28 12:20:25 +0200

    docker + postgres setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ForumModule } from './forum/forum.module';
 import { ChatModule } from './chat/chat.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [ForumModule, ChatModule],
+  imports: [
+    ForumModule,
+    ChatModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
