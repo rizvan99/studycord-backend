6f3ee2938f0a6d32a379af2bffc496973a20e64f
math231k <54638811+math231k@users.noreply.github.com>
2021-04-29 17:27:14 +0200

    First itteration of backend -no interface-

--- src/forum/api/gateway/forum.gateway.ts
+++ src/forum/api/gateway/forum.gateway.ts
@@ -1,11 +1,26 @@
 import {
+  ConnectedSocket,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { CategoryService } from '../../core/service/category.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ForumGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private categoryService: CategoryService) {}
+
+  @WebSocketServer() server;
+
+  @SubscribeMessage('getAllCategories')
+  async handleGetStocks(@ConnectedSocket() client: Socket): Promise<void> {
+    const allCategories = await this.categoryService.getCategories();
+    this.server.emit('category-getAll', allCategories);
+  }
+
   handleConnection(client: any, ...args: any[]): any {
     return null;
   }
--- src/forum/core/service/category.service.ts
+++ src/forum/core/service/category.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from '../model/category.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import CategoryDb from '../../infrastructure/data-source/entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(CategoryDb)
+    private categoryReposistory: Repository<CategoryDb>,
+  ) {}
+  async getCategories(): Promise<Category[]> {
+    const categories = await this.categoryReposistory.find();
+    const categoiresDb: Category[] = JSON.parse(JSON.stringify(categories));
+    return categoiresDb;
+  }
+}
