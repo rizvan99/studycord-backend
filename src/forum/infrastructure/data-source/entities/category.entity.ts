0fed703b869e37731bf8019d097f83542820e09b
math231k <54638811+math231k@users.noreply.github.com>
2021-04-29 11:36:37 +0200

    added category table to db

--- src/forum/infrastructure/data-source/entities/category.entity.ts
+++ src/forum/infrastructure/data-source/entities/category.entity.ts
@@ -1,9 +1,11 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 class CategoryDb {
-
-
+  @PrimaryGeneratedColumn()
+  public id: number;
+  @Column()
+  public name: string;
 }
 
 export default CategoryDb;
